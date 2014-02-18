d3439074ebabc3f7676685ec24a74530dc71e72c
Matt Collins <matt@scoot.co.uk>
2014-02-18 13:12:07 +0000

    confirmation page tweaks

--- views/confirmation.php
+++ views/confirmation.php
@@ -8,15 +8,11 @@
         <br>
       </div>
       
-      <div class="col-lg-8 mb60">
+      <div class="col-lg-12 mb60">
         <h2 class="mt40 white">Confirmation</h2>
-        <p>We have sent you a confirmation email to <strong><?=$_SESSION['confirmation_email'];?></strong>.</p>
-        <p>Simply find this email (you may have to check your spam box!) and click the link we provided in order to get going. Start using ScheduleSMS now!</p>
+        <p>Your account is now verified.</p>
+        <p>You can now <a href="/sign-in">Sign-in</a> to your ScheduleSMS dashboard and get started!</p>
         <p><a href="/sign-in" class="btn btn-success btn-lg"><i class="fa fa-sign-in"></i> Sign-in now!</a></p>
-      </div>
-
-      <div class="col-lg-4 mb60">
-        <i class="fa fa-8x mt40 fa-thumbs-up icon-green"></i>
       </div>
 
       
