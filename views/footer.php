b75f7598b4c07d8841dbd082a71c8298c912e6e1
Matt Collins <matt@scoot.co.uk>
2014-03-12 11:44:25 +0000

    load fonts async

--- views/footer.php
+++ views/footer.php
@@ -23,5 +23,32 @@
         interval: 5000
       })
     </script>
+    <script type="text/javascript">
+      // font awesome
+      (function() {
+        var css = document.createElement('link');
+        css.href = '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css';
+        css.rel = 'stylesheet';
+        css.type = 'text/css';
+        document.getElementsByTagName('head')[0].appendChild(css);
+      })();
+
+      // fonts
+      (function() {
+        var css = document.createElement('link');
+        css.href = '//fonts.googleapis.com/css?family=Raleway:400,300,700';
+        css.rel = 'stylesheet';
+        css.type = 'text/css';
+        document.getElementsByTagName('head')[0].appendChild(css);
+      })();
+
+      (function() {
+        var css = document.createElement('link');
+        css.href = '//fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic';
+        css.rel = 'stylesheet';
+        css.type = 'text/css';
+        document.getElementsByTagName('head')[0].appendChild(css);
+      })();
+    </script>
   </body>
 </html>
--- views/header.php
+++ views/header.php
@@ -55,9 +55,9 @@
     <? endif; ?>
     
 
-    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
-    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
+    <!--<link href='//fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">-->
     
     <script src="/js/jquery.min.js"></script>
     <script src="/js/smoothscroll.js"></script>
